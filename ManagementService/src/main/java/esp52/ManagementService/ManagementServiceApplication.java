98204687a634f20385b74358caeaba5c6297c0aa
rafaelsa99 <rafa.sa_@hotmail.com>
2021-06-04 16:09:07 +0100

    Management Service Initialization

---
+++ ManagementService/src/main/java/esp52/ManagementService/ManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package esp52.ManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementServiceApplication.class, args);
+	}
+
+}
