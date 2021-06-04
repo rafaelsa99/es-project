9a98cdc2952582508d69e0a9f4eebb2908dee8de
rafaelsa99 <rafa.sa_@hotmail.com>
2021-06-04 13:45:49 +0100

    Vehicles Service initialization

---
+++ VehiclesService/src/main/java/esp52/VehiclesService/VehiclesServiceApplication.java
@@ -0,0 +1,13 @@
+package esp52.VehiclesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesServiceApplication.class, args);
+	}
+
+}
---
+++ VehiclesService/src/test/java/esp52/VehiclesService/VehiclesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package esp52.VehiclesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
