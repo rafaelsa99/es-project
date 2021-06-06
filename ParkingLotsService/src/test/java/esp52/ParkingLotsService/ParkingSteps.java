8453a99fb840a3b45e813eb1e0d83d8b867dcfd7
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 23:46:39 +0100

    Update ParkingSteps.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingSteps.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingSteps.java
@@ -15,7 +15,7 @@ class FreeSpace {
 
     static String FreeSpace(int today) {
         if (today <= 349 && today > 50) {
-            return "is totally free! ";
+            return "is totally free!";
         } else if (today <= 50 && today > 0) {
             return "has a reduced number of free parking spaces!";
         } else {
@@ -31,53 +31,91 @@ public class ParkingSteps extends ParkingLotsServiceApplicationTests {
     ParkingLotation p1 = pS.getTestPark(349, 7);
     ParkingLotation p2 = pS.getTestPark(25, 3);
     ParkingLotation p3 = pS.getTestPark(0, 0);
+
     public int idk1 = p1.getFree();
     public int idk2 = p2.getFree();
     public int idk3 = p3.getFree();
+    public int idk1d = p1.getDisabledfree();
+    public int idk2d = p2.getDisabledfree();
+    public int idk3d = p3.getDisabledfree();
 
-    private int n1;
-    private int n2;
     private String actualAnswer;
-    boolean test;
-    private int int1;
 
     /*-------------------------------------------------------------
     Scenario Outline: Park is or is not free
     -------------------------------------------------------------*/
-    @Given("Parks space is {int}")
-    public void free_spaces(int int1) {
-        this.int1 = int1;
+    @Given("Parks space is 349")
+    public void free_spaces_1() {
+    }
+
+    @Given("Parks space is between 50 and 1")
+    public void free_spaces_2() {
+    }
+
+    @Given("Parks space is 0")
+    public void free_spaces_3() {
     }
 
     @When("I ask whether there are free parks")
-    public void i_ask_whether_it_s_Friday_yet() {
+    public void fp1() {
         System.out.println("  Space is -> " + idk1);
-        actualAnswer = FreeSpace.FreeSpace(int1);
+        actualAnswer = FreeSpace.FreeSpace(idk1);
     }
 
-//    @When("I ask whether there are free parks")
-//    public void how_many_spaces() {
-//
-//        int freePark = 52;
-//
-//        if (freePark == 349) {
-//            System.out.println("Park space is -> " + freePark);
-//            test = true;
-//        } else {
-//            System.out.println("Park space is -> " + freePark);
-//            test = false;
-//        }
-//
-//    }
+    @When("I ask whether there are free parkss")
+    public void fp2() {
+        System.out.println("  Space is -> " + idk2);
+        actualAnswer = FreeSpace.FreeSpace(idk2);
+    }
+
+    @When("I ask whether there are free parksss")
+    public void fp3() {
+        System.out.println("  Space is -> " + idk3);
+        actualAnswer = FreeSpace.FreeSpace(idk3);
+    }
+
     @Then("I should be told that {string}")
     public void answer_is(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
 
-//        if (test == true) {
-//            System.out.println("Test Passed!");
-//        } else {
-//            System.out.println("Test Failed!");
-//        }
     }
 
+    /*-------------------------------------------------------------
+    Scenario Outline: Park is or is not free (Disabled People)
+    -------------------------------------------------------------*/
+    @Given("Disabled Parks space is 7")
+    public void free_spaces_1d() {
+    }
+
+    @Given("Disabled Parks space is between 3 and 1")
+    public void free_spaces_2d() {
+    }
+
+    @Given("Disabled Parks space is 0")
+    public void free_spaces_3d() {
+    }
+
+    @When("I ask whether there are free disabled parks")
+    public void fp1d() {
+        System.out.println("  Space is -> " + idk1d);
+        actualAnswer = FreeSpace.FreeSpace(idk1d);
+    }
+
+    @When("I ask whether there are free disabled parkss")
+    public void fp2d() {
+        System.out.println("  Space is -> " + idk2d);
+        actualAnswer = FreeSpace.FreeSpace(idk2d);
+    }
+
+    @When("I ask whether there are free disabled parksss")
+    public void fp3d() {
+        System.out.println("  Space is -> " + idk3d);
+        actualAnswer = FreeSpace.FreeSpace(idk3d);
+    }
+
+    @Then("I should be told that {string}")
+    public void answer_isd(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+
+    }
 }
