7253dc66a2c00fef01a70d4d7c391c03b1f90cb5
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 18:38:26 +0100

    Update ParkingSteps.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingSteps.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingSteps.java
@@ -4,6 +4,46 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class ParkingSteps {
-	
+public class ParkingSteps extends SpringIntegrationTest {
+
+    private int n1;
+    private int n2;
+    boolean test;
+
+    /*-------------------------------------------------------------
+    Scenario Outline: Park is or is not free
+    -------------------------------------------------------------*/
+    @Given("Parks space is between {int} and {int}")
+    public void free_spaces(int int1, int int2) {
+
+        this.n1 = int1;
+        this.n2 = int2;
+
+    }
+
+    @When("I ask whether there are free parks")
+    public void how_many_spaces() {
+
+        int freePark = 2;
+
+        if (freePark <= n1 && freePark >= n2) {
+            System.out.println("Park space is -> " + freePark);
+            test = true;
+        } else {
+            System.out.println("Park space is -> " + freePark);
+            test = false;
+        }
+
+    }
+
+    @Then("I should be told that {string}")
+    public void answer_is(String string) {
+        if (test == true) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!");
+        }
+    }
+
+    //asserEqual (string, message)
 }
