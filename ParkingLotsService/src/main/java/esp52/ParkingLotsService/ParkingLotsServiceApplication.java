ab5a6ab9bd0b929bb84283f23653494a0a0a212c
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 19:42:35 +0100

    Update ParkingLotsServiceApplication.java

--- ParkingLotsService/src/main/java/esp52/ParkingLotsService/ParkingLotsServiceApplication.java
+++ ParkingLotsService/src/main/java/esp52/ParkingLotsService/ParkingLotsServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ParkingLotsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingLotsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotsServiceApplication.class, args);
+    }
 
 }
