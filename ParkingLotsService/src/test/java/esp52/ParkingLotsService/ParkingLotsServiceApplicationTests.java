ce1a4e0273b37eb902e9768596e44cdab1d19f84
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 18:38:14 +0100

    Update ParkingLotsServiceApplicationTests.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ParkingLotsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
