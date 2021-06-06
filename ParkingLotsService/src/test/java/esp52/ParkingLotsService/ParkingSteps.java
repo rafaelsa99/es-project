559d96aeee2ae679f3d6db8dcee1ce8f8ded693c
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 23:51:30 +0100

    Update ParkingSteps.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingSteps.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingSteps.java
@@ -23,6 +23,16 @@ class FreeSpace {
         }
     }
 
+    static String FreeDisabledSpace(int today2) {
+        if (today2 <= 7 && today2 >= 3) {
+            return "for disabled is totally free!";
+        } else if (today2 <= 2 && today2 >= 1) {
+            return "has a reduced number of free parking spaces for disabled!";
+        } else {
+            return "has the disabled parking spaces full!";
+        }
+    }
+
 }
 
 public class ParkingSteps extends ParkingLotsServiceApplicationTests {
@@ -58,19 +68,19 @@ public class ParkingSteps extends ParkingLotsServiceApplicationTests {
 
     @When("I ask whether there are free parks")
     public void fp1() {
-        System.out.println("  Space is -> " + idk1);
+        System.out.println("  Park space is -> " + idk1);
         actualAnswer = FreeSpace.FreeSpace(idk1);
     }
 
     @When("I ask whether there are free parkss")
     public void fp2() {
-        System.out.println("  Space is -> " + idk2);
+        System.out.println("  Park space is -> " + idk2);
         actualAnswer = FreeSpace.FreeSpace(idk2);
     }
 
     @When("I ask whether there are free parksss")
     public void fp3() {
-        System.out.println("  Space is -> " + idk3);
+        System.out.println("  Park space is -> " + idk3);
         actualAnswer = FreeSpace.FreeSpace(idk3);
     }
 
@@ -97,23 +107,23 @@ public class ParkingSteps extends ParkingLotsServiceApplicationTests {
 
     @When("I ask whether there are free disabled parks")
     public void fp1d() {
-        System.out.println("  Space is -> " + idk1d);
-        actualAnswer = FreeSpace.FreeSpace(idk1d);
+        System.out.println("  Disabled park space is -> " + idk1d);
+        actualAnswer = FreeSpace.FreeDisabledSpace(idk1d);
     }
 
     @When("I ask whether there are free disabled parkss")
     public void fp2d() {
-        System.out.println("  Space is -> " + idk2d);
-        actualAnswer = FreeSpace.FreeSpace(idk2d);
+        System.out.println("  Disabled park space is -> " + idk2d);
+        actualAnswer = FreeSpace.FreeDisabledSpace(idk2d);
     }
 
     @When("I ask whether there are free disabled parksss")
     public void fp3d() {
-        System.out.println("  Space is -> " + idk3d);
-        actualAnswer = FreeSpace.FreeSpace(idk3d);
+        System.out.println("  Disabled park space is -> " + idk3d);
+        actualAnswer = FreeSpace.FreeDisabledSpace(idk3d);
     }
 
-    @Then("I should be told that {string}")
+    @Then("I should be told {string}")
     public void answer_isd(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
 
