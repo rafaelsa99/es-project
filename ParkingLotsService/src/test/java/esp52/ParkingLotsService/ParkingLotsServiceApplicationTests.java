a3ac879fe09d65337f9d99ca991f066ece7db20a
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 19:39:23 +0100

    Update ParkingLotsServiceApplicationTests.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
@@ -1,10 +1,14 @@
 package esp52.ParkingLotsService;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,18 +19,59 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @CucumberContextConfiguration
+@ContextConfiguration
 class ParkingLotsServiceApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @Test
+//    public void getReact() throws Exception {
+//        System.out.println("Request React");
+//        this.mockMvc.perform(get("http://localhost:8083/lotations/")).andDo(print()).andExpect(status().isOk());
+//    }
+    private int n1;
+    private int n2;
+    boolean test;
 
-    @Test
-    void contextLoads() {
+    /*-------------------------------------------------------------
+    Scenario Outline: Park is or is not free
+    -------------------------------------------------------------*/
+    @Given("Parks space is between {int} and {int}")
+    public void free_spaces(int int1, int int2) {
+
+        this.n1 = int1;
+        this.n2 = int2;
+
     }
 
-    @Test
-    public void getReact() throws Exception {
-        System.out.println("Request React");
-        this.mockMvc.perform(get("http://localhost:8083/lotations/")).andDo(print()).andExpect(status().isOk());
+    @When("I ask whether there are free parks")
+    public void how_many_spaces() {
+
+        int freePark = 2;
+
+        if (freePark <= n1 && freePark >= n2) {
+            System.out.println("Park space is -> " + freePark);
+            test = true;
+        } else {
+            System.out.println("Park space is -> " + freePark);
+            test = false;
+        }
+
     }
+
+    @Then("I should be told that {string}")
+    public void answer_is(String string) {
+        if (test == true) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!");
+        }
+    }
+
+    //asserEqual (string, message)
 }
