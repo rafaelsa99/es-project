c63666e049476cd48d304914832769ac5bb80413
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 22:12:39 +0100

    Update CucumberIntegrationTest.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/CucumberIntegrationTest.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/CucumberIntegrationTest.java
@@ -9,9 +9,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources", glue = {"com.whatever"})
+//@RunWith(Cucumber.class)
+//@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources", glue = {"com.whatever"})
 //@CucumberOptions(features = "src/test/resources", plugin = {"pretty"}, publish = true)
-public class CucumberIntegrationTest extends ParkingLotsServiceApplicationTests {
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources",
+        plugin = {"pretty", "html:target/cucumber/bagbasics"},
+        extraGlue = "io.tpd.springbootcucumber.bagcommons")
+public class CucumberIntegrationTest {
 
 }
