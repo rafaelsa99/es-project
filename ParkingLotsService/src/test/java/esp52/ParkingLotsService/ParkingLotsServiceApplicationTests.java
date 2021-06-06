d202571aaa6ae3c73b725416ad43722c443d765a
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 19:48:23 +0100

    Update ParkingLotsServiceApplicationTests.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
@@ -25,53 +25,13 @@ class ParkingLotsServiceApplicationTests {
 //    @Autowired
 //    private MockMvc mockMvc;
 //
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 //
 //    @Test
 //    public void getReact() throws Exception {
 //        System.out.println("Request React");
 //        this.mockMvc.perform(get("http://localhost:8083/lotations/")).andDo(print()).andExpect(status().isOk());
 //    }
-    private int n1;
-    private int n2;
-    boolean test;
-
-    /*-------------------------------------------------------------
-    Scenario Outline: Park is or is not free
-    -------------------------------------------------------------*/
-    @Given("Parks space is between {int} and {int}")
-    public void free_spaces(int int1, int int2) {
-
-        this.n1 = int1;
-        this.n2 = int2;
-
-    }
-
-    @When("I ask whether there are free parks")
-    public void how_many_spaces() {
-
-        int freePark = 2;
-
-        if (freePark <= n1 && freePark >= n2) {
-            System.out.println("Park space is -> " + freePark);
-            test = true;
-        } else {
-            System.out.println("Park space is -> " + freePark);
-            test = false;
-        }
-
-    }
-
-    @Then("I should be told that {string}")
-    public void answer_is(String string) {
-        if (test == true) {
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed!");
-        }
-    }
-
-    //asserEqual (string, message)
 }
