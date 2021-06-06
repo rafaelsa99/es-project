f36023b506eb2bb94bedcde211c7c9a47e2cef03
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 19:11:15 +0100

    Update ParkingLotsServiceApplicationTests.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package esp52.ParkingLotsService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ParkingLotsServiceApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void getReact() throws Exception {
+        System.out.println("Request React");
+        this.mockMvc.perform(get("http://localhost:8083/lotations/")).andDo(print()).andExpect(status().isOk());
+    }
 }
