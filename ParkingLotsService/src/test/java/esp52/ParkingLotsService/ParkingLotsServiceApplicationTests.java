1bf8686c6140a140ba98f160c9c1f829e71f3044
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 19:22:04 +0100

    Update ParkingLotsServiceApplicationTests.java

--- ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/ParkingLotsServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package esp52.ParkingLotsService;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@CucumberContextConfiguration
 class ParkingLotsServiceApplicationTests {
 
     @Autowired
