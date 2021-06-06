35d984bc8e59a27ff54704d7c6c45a1a9ff75adc
Diogo Miguel Azevedo <dma23@ua.pt>
2021-06-06 19:22:02 +0100

    Create CucumberIntegrationTest.java

---
+++ ParkingLotsService/src/test/java/esp52/ParkingLotsService/CucumberIntegrationTest.java
@@ -0,0 +1,17 @@
+package esp52.ParkingLotsService;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+//@CucumberOptions(features = "src/test/resources", plugin = {"pretty"}, publish = true)
+public class CucumberIntegrationTest extends ParkingLotsServiceApplicationTests {
+
+}
